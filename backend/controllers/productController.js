c136642225c7ca5913c0d8f56220c688e5c0f0c7
CodingStuff-Github <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-06-24 19:35:40 +0530

    Update productController.js
    Find all Products API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,18 +1,19 @@
 import Product from "../models/productModel.js";
 
-// Get all products for the current route
-export const getAllProducts = (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Route is working successfully",
-  });
-};
-
 // Create a new product
 export const createProduct = async (req, res) => {
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
     product,
+  });
+};
+
+// Get all products
+export const getAllProducts = async (_req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
   });
 };

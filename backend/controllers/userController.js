f491c4250781e3ea55126baa0124dcd5e5e90df8
CodingStuff-Github <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-06-27 17:58:05 +0530

    register User API

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+import asyncPromiseError from "../middleware/asyncPromiseError.js";
+import User from "../models/userModel.js";
+
+export const registerUser = asyncPromiseError(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: password,
+    avatar: { public_id: "Sample_ID", url: "Sample_URL" },
+  });
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});

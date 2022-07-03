488098160d51caa55b1ba35991db14578b00cd4b
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-07-03 23:39:12 +0530

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,5 +3,7 @@ import { createOrder } from "../controllers/orderController.js";
 
 export const router = express.Router();
 
+import { isAuthenticated, AuthorizedRoles } from "../middleware/auth.js";
+
 // Creates a new order
-router.route("/order").post(createOrder);
+router.route("/order").post(isAuthenticated, createOrder);

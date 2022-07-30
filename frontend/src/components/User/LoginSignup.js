797bc047a584ee983eb315cd611fcb3a7b2d703f
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-07-30 12:09:09 +0530

    Update LoginSignup.js

--- frontend/src/components/User/LoginSignup.js
+++ frontend/src/components/User/LoginSignup.js
@@ -1,85 +1,184 @@
-import React from "react";
-import logo from "../../images/logo.svg";
+import React, { useRef } from "react";
+
 const LoginSignup = () => {
+  const loginButton = useRef(null);
+  const loginForm = useRef(null);
+  const registerButton = useRef(null);
+  const registerForm = useRef(null);
+
+  const switchTabs = (e, tab) => {
+    if (tab === "login") {
+      loginButton.current.classList.add("border-b-2", "border-indigo-500");
+      registerButton.current.classList.remove(
+        "border-b-2",
+        "border-indigo-500"
+      );
+      loginForm.current.classList.add("-translate-x-full");
+    }
+    if (tab === "register") {
+      registerButton.current.classList.add("border-b-2", "border-indigo-500");
+      loginButton.current.classList.remove("border-b-2", "border-indigo-500");
+      loginForm.current.classList.add("translate-x-0");
+    }
+  };
+
   return (
     <>
-      <section class="bg-gray-50 dark:bg-gray-900 py-8">
-        <div class="flex flex-col items-center justify-center mx-auto ">
+      <section className="bg-gray-50 dark:bg-gray-900 py-8">
+        <div className="flex flex-col items-center justify-center mx-auto ">
           {/* Login and Signup Card */}
-          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             {/* Login and Register Options */}
-            <div class="w-full inline-flex rounded-md shadow-sm" role="group">
+            <div
+              className="w-full inline-flex rounded-md shadow-sm"
+              role="group"
+            >
               <button
+                ref={registerButton}
                 type="button"
-                class="w-full py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+                className="border-b-2 border-indigo-500 w-full py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg hover:bg-gray-100 hover:text-blue-700"
+                onClick={(e) => {
+                  switchTabs(e, "register");
+                }}
               >
                 Register
               </button>
 
               <button
                 type="button"
-                class="w-full py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+                ref={loginButton}
+                className="w-full py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg hover:bg-gray-100 hover:text-blue-700"
+                onClick={(e) => {
+                  switchTabs(e, "login");
+                }}
               >
                 Login
               </button>
             </div>
-            {/* Form Div */}
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
-              </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
-                {/* Email */}
-                <div>
-                  <label
-                    for="email"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+
+            <div className="flex overflow-hidden">
+              {/* Login Form Div */}
+              <div
+                ref={loginForm}
+                className="w-full transition p-6 space-y-4 md:space-y-6 sm:p-8"
+              >
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign in to your account
+                </h1>
+                <form className="space-y-4 md:space-y-6" action="#">
+                  {/* Email */}
+                  <div>
+                    <label
+                      for="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="name@company.com"
+                      required=""
+                    />
+                  </div>
+                  {/* Password */}
+                  <div>
+                    <label
+                      for="password"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required=""
+                    />
+                  </div>
+                  {/* Forget Password Link */}
+                  <div className="grid justify-items-end">
+                    <a
+                      href="#"
+                      className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >
+                      Forgot password?
+                    </a>
+                  </div>
+                  {/* Submit Button */}
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-blue-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                   >
-                    Your email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
-                    required=""
-                  />
-                </div>
-                {/* Password */}
-                <div>
-                  <label
-                    for="password"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    Sign in
+                  </button>
+                </form>
+              </div>
+              {/* Register Form Div */}
+              <div
+                ref={registerForm}
+                className="transition p-6 space-y-4 md:space-y-6 sm:p-8"
+              >
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Create your account
+                </h1>
+                <form className="space-y-4 md:space-y-6" action="#">
+                  {/* Email */}
+                  <div>
+                    <label
+                      for="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="name@company.com"
+                      required=""
+                    />
+                  </div>
+                  {/* Password */}
+                  <div>
+                    <label
+                      for="password"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required=""
+                    />
+                  </div>
+                  {/* Forget Password Link */}
+                  <div className="grid justify-items-end">
+                    <a
+                      href="#"
+                      className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >
+                      Forgot password?
+                    </a>
+                  </div>
+                  {/* Submit Button */}
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-blue-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                   >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
-                  />
-                </div>
-                {/* Forget Password Link */}
-                <div class="grid justify-items-end">
-                  <a
-                    href="#"
-                    class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-                {/* Submit Button */}
-                <button
-                  type="submit"
-                  class="w-full text-white bg-blue-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                >
-                  Sign in
-                </button>
-              </form>
+                    Sign in
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>

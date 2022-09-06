c97f337e77f4defe1ab984d9d33e826335aca536
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-09-06 18:14:05 +0530

    Update Home.js

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,17 +1,40 @@
 import React, { Fragment, useEffect } from "react";
 import Hero from "../layout/Hero/Hero";
+import { useSearchParams } from "react-router-dom";
 import ProductCard from "./ProductCard.js";
 import Metadata from "../layout/Metadata";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchProducts } from "../../state_management/product/productSlice";
 import Loader from "../layout/Loader/Loader";
 import ErrorView from "../layout/ErrorPage/ErrorView";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const Home = () => {
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  const redirectStatus = searchParams.get("redirect_status");
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   const { loading, products, err } = useSelector((state) => state.productStore);
 
   useEffect(() => {
+    if (redirectStatus === "succeeded") {
+      setOpen(true);
+    }
     dispatch(fetchProducts({}));
   }, [dispatch]);
 
@@ -21,6 +44,34 @@ const Home = () => {
       {!loading && products ? (
         <>
           <Metadata title="ExOFusion" />
+          <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+            <Alert
+              onClose={handleClose}
+              severity="success"
+              sx={{ width: "100%" }}
+              iconMapping={{
+                success: (
+                  <svg
+                    aria-hidden="true"
+                    class="flex-shrink-0 inline w-5 h-5 mr-3"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                ),
+              }}
+            >
+              <span class="font-bold">Order Placed Successfully!</span> Thank
+              you for placing an order.{" "}
+              <p>You can track status of your orders in Orders page.</p>
+            </Alert>
+          </Snackbar>
           <Hero />
           {/* Heading */}
           <div className="lg:px-20 md:px-6 px-4 md:py-12 py-8">

a2aa19cfe0311da4976d9f43db15b4c63b2301dd
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-09-10 05:08:29 +0530

    getPublishableKey API

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,10 +1,5 @@
 import asyncPromiseError from "../middleware/asyncPromiseError.js";
 import stripe from "stripe";
-import dotenv from "dotenv";
-
-dotenv.config({ path: "backend/config/config.env" });
-
-const stripeUse = stripe(process.env.STRIPE_API_SECRET);
 
 const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
@@ -15,19 +10,27 @@ const calculateOrderAmount = (items) => {
 
 export const createPaymentIntent = asyncPromiseError(
   async (req, res, _next) => {
+    const stripeUseSecret = stripe(process.env.STRIPE_API_SECRET);
     const { items } = req.body;
-
+    //No items send error response
     // Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await stripeUse.paymentIntents.create({
+    const paymentIntent = await stripeUseSecret.paymentIntents.create({
       amount: calculateOrderAmount(items),
       currency: "inr",
       automatic_payment_methods: {
         enabled: true,
       },
     });
-
-    res.send({
+    res.status(200).json({
+      success: true,
       clientSecret: paymentIntent.client_secret,
     });
   }
 );
+
+export const getPublishableKey = asyncPromiseError(async (_req, res, _next) => {
+  res.status(200).json({
+    success: true,
+    publishableKey: process.env.STRIPE_API_PUBLISHABLE,
+  });
+});
--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { createPaymentIntent } from "../controllers/paymentController.js";
+import {
+  createPaymentIntent,
+  getPublishableKey,
+} from "../controllers/paymentController.js";
 import { isAuthenticated, AuthorizedRoles } from "../middleware/auth.js";
 
 export const router = express.Router();
+
 router
   .route("/create-payment-intent")
   .post(isAuthenticated, AuthorizedRoles("admin", "user"), createPaymentIntent);
+
+router
+  .route("/get-publishable-key")
+  .get(isAuthenticated, AuthorizedRoles("admin", "user"), getPublishableKey);
